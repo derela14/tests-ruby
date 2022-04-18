cddcd6ab4f08e4efcb8d4ea6bbe522bcb04df22a
derela14 <lau13perdereau@gmail.com>
2022-04-18 23:21:29 +0200

    code final

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -5,3 +5,27 @@ end
 def greet(name)
 
 end
+#code de départ
+def hello
+    puts "Hello !"
+end
+hello
+
+def ask_first_name
+    puts "what's your name?"
+    print "> "
+    first_name = gets.chomp
+    return first_name
+  end
+  
+  
+  def greet(first_name)
+    puts "Hello #{first_name}!"
+  end
+  
+  def perform
+    first_name = ask_first_name
+    greet(first_name)
+  end
+  
+  perform
